6ceda0c704e4daae3b9fb22edc43c642879f264c
vpasspro <vpassprosolution@gmail.com>
2025-03-28 05:35:38 +0800

    fix

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from fastapi import FastAPI
-from webhook import router as webhook_router
+from webhook import router as webhook_router  # import the webhook router
 
 app = FastAPI()
+
+# include the webhook router
 app.include_router(webhook_router)
